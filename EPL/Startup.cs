01dbfa276f39e6cb1026279ecb4862305634d024
tan-xianglong <t.xianglong@gmail.com>
2022-04-29 21:54:39 +0800

    Fixed DbContext error, Add non function search at homepage

--- EPL/Startup.cs
+++ EPL/Startup.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EPL.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,14 @@ namespace EPL
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DbContextConnection")));
+            
+
+            services.AddScoped<IDivisionRepository, MockDivisionRepository>();
+            services.AddScoped<ITeamRepository, MockTeamRepository>();
+            services.AddScoped<IPlayerRepository, MockPlayerRepository>();
+
             services.AddHttpContextAccessor();
             services.AddSession();
             services.AddControllersWithViews();
