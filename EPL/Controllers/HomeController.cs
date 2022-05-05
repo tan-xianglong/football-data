c067036f47d12ee9338db616554bdd1a7b137384
tan-xianglong <t.xianglong@gmail.com>
2022-05-06 00:27:07 +0800

    Add homepage multidata search feature

--- EPL/Controllers/HomeController.cs
+++ EPL/Controllers/HomeController.cs
@@ -6,21 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EPL.Models;
+using EPL.ViewModels;
 
 namespace EPL.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IPlayerRepository playerRepository;
+        private readonly ITeamRepository teamRepository;
+        private readonly IDivisionRepository divisionRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IPlayerRepository playerRepository, ITeamRepository teamRepository, IDivisionRepository divisionRepository)
         {
             _logger = logger;
+            this.playerRepository = playerRepository;
+            this.teamRepository = teamRepository;
+            this.divisionRepository = divisionRepository;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index(HomeIndexViewModel homeIndexViewModel)
+        {   
+            var players = playerRepository.GetPlayersByName(homeIndexViewModel.SearchTerm);
+            var teams = teamRepository.GetTeamsByName(homeIndexViewModel.SearchTerm, null);
+            var divisions = divisionRepository.GetDivisionByName(homeIndexViewModel.SearchTerm);
+            return View(new HomeIndexViewModel
+            {
+                Players = players,
+                Teams = teams,
+                Divisions = divisions
+            });
         }
 
         public IActionResult Privacy()
---
+++ EPL/ViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using EPL.Models;
+
+namespace EPL.ViewModels
+{
+	public class HomeIndexViewModel
+	{
+		public IEnumerable<Player> Players { get; set; }
+
+
+		public IEnumerable<Team> Teams { get; set; }
+
+        public IEnumerable<Division> Divisions { get; set; }
+
+        public string SearchTerm { get; set; }
+
+        public HomeIndexViewModel()
+        {
+            SearchTerm = "Search name";
+        }
+	}
+}
+
